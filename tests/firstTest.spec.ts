233d87d568a04511398998ff97e8438ea5611981
Ricardo Ferreira <rfsferreira@pm.me>
2024-09-29 20:49:45 +0100

    Added comment for approval of pr

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -46,7 +46,7 @@ test('User facing locators', async ({page}) => {
 test('locating child elements', async ({page}) => {
     await page.locator('nb-card nb-radio :text-is("Option 1")').click()
     await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
-
+    //Added some comments to have pr approved
     await page.locator('nb-card').getByRole('button', {name: 'Sign in'}).first().click()
 
     await page.locator('nb-card').nth(0).getByRole('button').click()
