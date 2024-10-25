caa11ab62e95207316514a8309539169db2db243
Ricardo Ferreira <rfsferreira@pm.me>
2024-10-25 18:41:08 +0100

    Added radio input tests

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -1,20 +1,20 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test.beforeEach(async ({page}) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
 })
 
 test.describe('Form Layouts page', () => {
-    test.beforeEach(async ({page}) => {
+    test.beforeEach(async ({ page }) => {
         await page.getByText('Forms').click()
         await page.getByText('Form Layouts').click()
     })
 
-    test('input fields', async ({page}) => {
-        const usingTheGridEmailInput = page.locator('nb-card', {hasText: 'Using the grid'}).getByRole('textbox', {name: 'Email'})
+    test('input fields', async ({ page }) => {
+        const usingTheGridEmailInput = page.locator('nb-card', { hasText: 'Using the grid' }).getByRole('textbox', { name: 'Email' })
         await usingTheGridEmailInput.fill('test@example.com')
         await usingTheGridEmailInput.clear()
-        await usingTheGridEmailInput.pressSequentially('test2@example.com', {delay: 100})
+        await usingTheGridEmailInput.pressSequentially('test2@example.com', { delay: 100 })
 
         //generic assertion
         const inputValue = await usingTheGridEmailInput.inputValue()
@@ -22,5 +22,18 @@ test.describe('Form Layouts page', () => {
 
         //locator assertion
         await expect(usingTheGridEmailInput).toHaveValue('test2@example.com')
+    })
+    test('radio buttons', async ({ page }) => {
+        const usingTheGridForm = page.locator('nb-card', { hasText: 'Using the grid' })
+
+        //await usingTheGridForm.getByLabel('Option 1').check({force: true})
+        await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).check({force: true})
+        const radioStatus =await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()
+        expect(radioStatus).toBeTruthy()
+        await expect(usingTheGridForm.getByRole('radio', {name: 'Option 1'})).toBeChecked()
+
+        await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).check({force: true})
+        expect(await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()).toBeFalsy()
+        expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy()
     })
 })
