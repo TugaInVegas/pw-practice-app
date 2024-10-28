a938380f4f8b9c0099e89e34f709f284c45d169c
Ricardo Ferreira <rfsferreira@pm.me>
2024-10-28 10:54:56 +0000

    Added checkbox tests

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -37,3 +37,20 @@ test.describe('Form Layouts page', () => {
         expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy()
     })
 })
+
+test('checkboxes', async ({ page }) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    // click simulates a mouse click, whereas check specifically toggles a checkbox's state.
+    await page.getByRole('checkbox', {name: 'Hide on click'}).click({force: true})
+    await page.getByRole('checkbox', {name: 'Hide on click'}).uncheck({force: true})
+    await page.getByRole('checkbox', {name: 'Prevent arising of duplicate toast'}).check({force: true})
+
+    //loop through all checkboxes and uncheck them
+    const allBoxes = page.getByRole('checkbox')
+    for(const box of await allBoxes.all()){
+        await box.uncheck({force: true})
+        expect(await box.isChecked()).toBeFalsy()
+    }
+})
