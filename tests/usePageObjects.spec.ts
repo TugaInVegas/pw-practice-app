831446fb536fe92c4ba5cbdbd4601a968a280b4f
Ricardo Ferreira <rfsferreira@pm.me>
2024-11-22 14:09:09 +0000

    Datepicker lesson

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test'
 import { NavigationPage } from '../page-objects/navigationPage'
 import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
-
+import { DatepickerPage } from '../page-objects/datepickerPage'
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200')
 })
@@ -18,8 +18,13 @@ test('navigate to form page', async({page}) => {
 test('parameterized methods', async({page}) => {
     const navigateTo = new NavigationPage(page)
     const onFormLayoutsPage = new FormLayoutsPage(page)
+    const onDatepickerPage = new DatepickerPage(page)
 
     await navigateTo.formLayoutsPage()
     await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 1')
     await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Smith', 'john.smith@example.com', false)
+
+    await navigateTo.datepickerPage()
+    await onDatepickerPage.selectCommonDatePickerDateFromToday(5)
+    await onDatepickerPage.selectDatePickerWithRangeFromToday(6, 10)
 })
